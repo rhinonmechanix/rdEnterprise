752ceb677808d3061740232fee2c4420467b93be
Prabhat <prabhat@uexcelerate.com>
2024-02-05 00:19:24 +0530

    new changes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+
+  constructor(private route: Router) {}
+  
+  goToFutureInterio(){
+    this.route.navigate(['/futureInterio']);
+  }
 
 }
